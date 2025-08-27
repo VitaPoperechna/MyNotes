4b527278489233d20652a5d7b3959849a904b62a
VitaPoperechna <vitapoperechna@gmal.com>
2025-08-27 14:22:24 +0200

    feat: added UserService class

--- src/main/java/com/femcoders/mynotes/repositories/UserRepository.java
+++ src/main/java/com/femcoders/mynotes/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.femcoders.mynotes.repositories;
 import com.femcoders.mynotes.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<Object> findByUsername(String username);
 }
